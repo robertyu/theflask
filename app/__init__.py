177789dc2de2f4eed5dbe146e6f1be50e37962c3
root <root@rolin.trendisdcs.org>
2016-06-13 12:27:46 +0800

    Seventh commit

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from config import basedir
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
